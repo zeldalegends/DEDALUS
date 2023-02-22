74c52f555fb8e29d0ded821571849a905b7fe97f
Smart Energy Projects <114106111+engsep@users.noreply.github.com>
2023-02-22 01:32:25 +0100

    Commissioned to FIWARE Lab VM

---
+++ start_on_windows.bat
@@ -0,0 +1,17 @@
+@echo off
+rem set COMPOSE_PROJECT_NAME=fiware
+
+call setenv.bat
+
+set DOCKER_FILE=docker-compose.yml
+set PROJECT_NAME=fiware
+
+rem docker compose -f %DOCKER_FILE% down
+rem docker compose -f %DOCKER_FILE% pull
+docker compose -f %DOCKER_FILE% -p %PROJECT_NAME% up --build --remove-orphans -d
+
+:loop
+docker compose -f %DOCKER_FILE% logs -t --until 1m
+docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
+pause
+goto loop
