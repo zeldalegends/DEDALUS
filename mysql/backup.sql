3496ed8b24424c837ae741c31e728cfd90fd2fa2
Smart Energy Projects <114106111+engsep@users.noreply.github.com>
2023-03-10 18:13:12 +0100

    Update backup.sql

--- mysql/backup.sql
+++ mysql/backup.sql
@@ -760,7 +760,7 @@ INSERT INTO `user_organization` VALUES
 (8,'member','detective1-good-0000-0000-000000000000','security-team-0000-0000-000000000000'),
 (9,'member','detective2-good-0000-0000-000000000000','security-team-0000-0000-000000000000'),
 (10,'owner','xxxxxxxx-good-0000-0000-000000000000','managers-team-0000-0000-000000000000'),
-(11,'owner','xxxxxxxx-good-0000-0000-000000000000','security-team-0000-0000-000000000000'),
+(11,'owner','xxxxxxxx-good-0000-0000-000000000000','security-team-0000-0000-000000000000');
 /*!40000 ALTER TABLE `user_organization` ENABLE KEYS */;
 UNLOCK TABLES;
 
