8b751c82e1ab879eed8380a326a1d483cb65db4c
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-12 03:53:15 +0000

    hyperride.loca.lt

--- start-on-linux.sh
+++ start-on-linux.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-echo "\033[0;32mFIWARE setup started.\033[0m"
+echo "\033[0;44m HYPERRIDE Setup \033[0m"
 # sudo chown -R 1000:1000 node-red
-docker compose pull
-echo "\033[0;32mDocker images updated.\033[0m"
+echo "\033[0;34mUpdating Docker images...\033[0m"
+docker compose pull && echo "\033[0;32mDocker images updated.\033[0m"
 # docker compose down -v --remove-orphans
-echo "\033[0;32mDocker containers restarting...\033[0m"
-docker compose -p fiware up -d --remove-orphans --build
-echo "\033[0;32mDocker containers restarted.\033[0m"
+echo "\033[0;34mBuilding Docker containers...\033[0m"
+docker compose -p fiware up -d --remove-orphans --build && echo "\033[0;32mDocker containers built.\033[0m"
 echo ""
-echo "\033[0;44m FIWARE Stack \033[0m"
-echo "\033[0;33mby Alerox 2023 \033[0m"
+echo "\033[0;44m ICT Platform \033[0m"
+echo "\033[0;33mby Alerox 2023\033[0m"
 echo ""
 node -e "console.log(require('os').release())"
 node -e "console.log(require('os').version())"
-ver=$(docker -v); echo "${ver//, build*/}"
+docker -v
 ver=$(node -v); echo "Node.js $ver"
 echo ""
-echo "\033[1;36mWelcome to FIWARE\033[0m"
+echo "\033[1;36mWelcome to HYPERRIDE!\033[0m"
 docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"
 echo ""
 echo "\033[0;36mNotify Node-RED app on Replit...\033[0m"
@@ -43,4 +42,4 @@ curl -X POST https://fiware.zeldalegends.repl.co/codespaces -H 'Content-Type: ap
 # Docker common commands
 # ----------------------
 # docker exec -it node-red /bin/bash
-# docker logs fiware-orion -f
+# docker logs fiware-orion --tail 10 -f
