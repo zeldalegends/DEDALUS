6cc09104a2356f2cfaf5b66975a9f539f6b10008
zeldalegends <gamesup@libero.it>
2023-11-29 05:36:35 +0000

    phpMyAdmin replaces adminer, all healthchecks set, curl PUT to public/uploads

--- list.bat
+++ list.bat
@@ -1,5 +1,6 @@
 @echo off
 
-docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"
+docker ps -a --format "table {{.Names}}\t{{.ID}}\t{{.Status}}\t{{.Ports}}\t{{.Image}}" | sort
+
 pause
 docker compose -p fiware logs --tail 10 -f
