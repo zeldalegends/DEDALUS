a6b141049094161dfb535253e9aa33bb98a8be13
zeldalegends <73659648+zeldalegends@users.noreply.github.com>
2024-04-08 06:10:35 +0200

    create .token in the same folder

--- upload-to-github.sh
+++ upload-to-github.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo -e "\033[0;36m🟦 CREATE ACCESS TOKEN \033[0m"
+echo -e "\033[0;36m🟩 CREATE ACCESS TOKEN \033[0m"
 echo To create a new access token, go to: https://github.com/settings/tokens/new && echo ""
 
-echo -e "\033[0;36m🟦 CLONE REPO AND CONFIG USER \033[0m"
+echo -e "\033[0;36m🟩 CLONE REPO AND CONFIG USER \033[0m"
 echo git clone https://github.com/zeldalegends/FIWARE
 echo git config --global user.email "gamesup@libero.it" && echo ""
 
-echo -e "\033[0;36m🟦 LOG STATUS AND COMMIT HISTORY \033[0m"
+echo -e "\033[0;36m🟩 LOG STATUS AND COMMIT HISTORY \033[0m"
 echo 'git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
 echo git status && echo ""
 
-echo -e "\033[0;36m🟦 COMMIT ALL CHANGES IN FOLDER \033[0m"
+echo -e "\033[0;36m🟩 COMMIT ALL CHANGES IN FOLDER \033[0m"
 git add .
 echo -e "\033[0;32mPress Enter to set commit title (CTRL+S+X to terminate)...\033[0m" && read key
 git commit && echo ""
