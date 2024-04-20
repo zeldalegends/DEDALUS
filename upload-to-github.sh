b2dfdf9529841ecbfc37768310e15182495572d4
zeldalegends <gamesup@libero.it>
2024-04-20 20:13:41 +0000

    first commit

--- upload-to-github.sh
+++ upload-to-github.sh
@@ -15,17 +15,11 @@
 git config user.email gamesup@libero.it
 git config user.name zeldalegends
 
-echo -e "\033[0;36m🟦 LOGS \033[0m"
-git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"
-read key
-echo -e "\033[0;36m🟦 STATUS \033[0m"
-git status
-read key
-
+# git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"
+# git status
 git add .
-git commit -m "commit for local changes"
-echo -e "\033[0;32m🟩 CREDENTIALS \033[0m"
+git commit -m "first commit"
+echo -e "\033[0;36m🟦 CREDENTIALS \033[0m"
 echo Username: zeldalegends
 echo Password: $(cat .token)
-cat .token | clip
 git push origin
